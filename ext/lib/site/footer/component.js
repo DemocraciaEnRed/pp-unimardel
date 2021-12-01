5d7d030ecc6b09bd90d11225ef552125f6f1cdd7
Mariano Valdez <marianovaldez92@protonmail.com>
2021-12-01 18:05:37 -0300

    agrego reglamento en footer

--- ext/lib/site/footer/component.js
+++ ext/lib/site/footer/component.js
@@ -32,7 +32,7 @@ const Footer = () => (
       <div className='terminos'>
         <Link to='/s/terminos-y-condiciones' tabIndex="13"> Términos y condiciones
         </Link>
-        <a href="https://participa.mardelplata.gob.ar/?page_id=1551" tabIndex="14" rel="noopener noreferer" target="_blank"> Reglamento
+        <a href="https://docs.google.com/document/d/1h3egOvtwpBDZHS2fY5Ms2dWP0YslU_pZ/edit?usp=sharing&ouid=104210888266272447846&rtpof=true&sd=true" tabIndex="14" rel="noopener noreferer" target="_blank"> Reglamento
         </a>
       </div>
     </div>
