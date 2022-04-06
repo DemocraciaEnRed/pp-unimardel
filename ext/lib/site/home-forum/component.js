76d332471dbf2e4d5fc7fc7305687f0d42b7a377
Mariano Valdez <marianovaldez92@protonmail.com>
2022-04-06 10:42:00 -0300

    cambio vista de HomePropuestas a HomeCatalogo

--- ext/lib/site/home-forum/component.js
+++ ext/lib/site/home-forum/component.js
@@ -1,16 +1,17 @@
 import React from 'react'
 //import HomeProyectos from '../home-proyectos/component'
-import HomePropuestas from '../home-propuestas/component'
+// import HomePropuestas from '../home-propuestas/component'
+import HomeCatalogo from '../home-catalogo/component'
 
 const HomeForum = (props) => {
   const { params: { forum } } = props
   switch (forum) {
     case 'propuestas':
-      return <HomePropuestas {...props} />
+      return <HomeCatalogo {...props} />
     default:
       // que nunca caiga en la vieja pantalla de proyectos
       //return <HomeProyectos {...props} />
-      return <HomePropuestas {...props} />
+      return <HomeCatalogo {...props} />
   }
 }
 
