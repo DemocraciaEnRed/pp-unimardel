a418090aefc71b3c90acb7c70befa76a433b075e
Mariano Valdez <marianovaldez92@protonmail.com>
2022-07-05 13:22:33 -0300

    arreglo responsive en votacion

--- ext/lib/site/formulario-votacion/steps/topic-card/component.js
+++ ext/lib/site/formulario-votacion/steps/topic-card/component.js
@@ -32,65 +32,62 @@ export class VotoTopicCard extends Component {
       return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
     }
 
-
-    return (
-      <div className={`voto-topic-card ${topic.id === selected && 'active'}`}>
-        <div className="row">
-          <div className="col-md-8">
-            <div className='voto-topic-card-info'>
-              <h1 className='voto-topic-card-title'>
-                {topic.mediaTitle.length > 70 ? topic.mediaTitle.slice(0,50) + "..." : topic.mediaTitle}
-              </h1>
-              {topic.attrs && <p className='voto-topic-card-description'>
-                {createClauses(topic)}
-              </p>}
-
-            </div>
-
-
-
-            <div className='voto-topic-card-footer'>
-              <div className='voto-topic-card-tags'>
-                { topic.zona && <span
-                  className='voto-tag-wrapper tag-zona' >
-                  {capitalizeFirstLetter(topic.zona.nombre)}
-                </span>
-                }                
-                { topic.tag && <span
-                    className='voto-tag-wrapper' >
-                    {capitalizeFirstLetter(topic.tag.name)}
-                  </span>   
-                }
-              </div>
-            </div>
-
-
-
-
-
-          </div>
-          {topic.attrs && topic.attrs['presupuesto-total'] && <div className="col-md-2">
-            <div className='voto-topic-card-info'>
-                <h1 className='voto-topic-card-title'>
-                  Presupuesto
+    if (window.matchMedia('(max-width: 768px)').matches) {
+      return (
+        <div className={`voto-topic-card ${topic.id === selected ? 'active' : ""}`}>
+          <div className="row">
+            <div className="col-xs-9">
+              <div className="voto-topic-card-info">
+                <h1>
+                  {topic.mediaTitle.length > 70 ? topic.mediaTitle.slice(0,50) + "..." : topic.mediaTitle}
                 </h1>
-                <p className='voto-topic-card-presupuesto'>
-                  ${topic.attrs['presupuesto-total'].toLocaleString()}
-                </p>
+                {topic.attrs && <p>{createClauses(topic)}</p>}
+                <div className='voto-topic-card-tags'>
+                  {topic.zona && <span className='voto-tag-wrapper tag-zona' >{capitalizeFirstLetter(topic.zona.nombre)}</span>}                
+                  {topic.attrs && topic.attrs['presupuesto-total'] && <span className='voto-tag-wrapper tag-presupuesto'>Presupuesto: ${topic.attrs['presupuesto-total'].toLocaleString()}</span>}
+                  {topic.tag && <span className='voto-tag-wrapper'>{capitalizeFirstLetter(topic.tag.name)}</span>}
+                </div>                
               </div>
-          </div>}
-          {handler && setState && <div className="col-md-2">
-            <div className="voto-topic-card-checkbox">
-              <input type="checkbox" name={handler} value={topic.id} onChange={setState} checked={topic.id === selected && true} className='select-topic' />
             </div>
-          </div>}
-          
-
+            {handler && setState && <div className="col-xs-3">
+              <div className="voto-topic-card-checkbox">
+                <input type="checkbox" name={handler} id={topic.id} value={topic.id} onChange={setState} checked={topic.id === selected && true} className='select-topic' />
+              </div>
+            </div>}
+          </div>
         </div>
-
-
-      </div>
-    )
+      )
+    } else {
+      return (
+        <div className={`voto-topic-card ${topic.id === selected ? 'active' : ""}`}>
+          <div className="row">
+            <div className="col-md-8">
+              <div className="voto-topic-card-info">
+                <h1>
+                  {topic.mediaTitle.length > 70 ? topic.mediaTitle.slice(0,50) + "..." : topic.mediaTitle}
+                </h1>
+                {topic.attrs && <p>{createClauses(topic)}</p>}
+                <div className='voto-topic-card-tags'>
+                  {topic.zona && <span className='voto-tag-wrapper tag-zona' >{capitalizeFirstLetter(topic.zona.nombre)}</span>}                
+                  {topic.tag && <span className='voto-tag-wrapper' >{capitalizeFirstLetter(topic.tag.name)}</span>}
+                </div>                
+              </div>
+            </div>
+            {topic.attrs && topic.attrs['presupuesto-total'] && <div className="col-md-2 text-center">
+              <div className='voto-topic-card-presupuesto'>
+                <h1>Presupuesto</h1>
+                <p className='superbold'>${topic.attrs['presupuesto-total'].toLocaleString()}</p>
+              </div>
+            </div>}
+            {handler && setState && <div className="col-md-2">
+              <div className="voto-topic-card-checkbox">
+                <input type="checkbox" name={handler} id={topic.id} value={topic.id} onChange={setState} checked={topic.id === selected && true} className='select-topic' />
+              </div>
+            </div>}
+          </div>
+        </div>
+      )
+    }
   }
 }
 
