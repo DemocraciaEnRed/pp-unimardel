d8ecaa801cc26ed3bd4b8035899e26225a0d0c04
Mariano Valdez <marianovaldez92@protonmail.com>
2022-07-15 17:52:03 -0300

    acentos

--- ext/lib/site/formulario-votacion/steps/bienvenida.js
+++ ext/lib/site/formulario-votacion/steps/bienvenida.js
@@ -5,12 +5,12 @@ export default ({changeStep}) => (
         <div className='votacion-header'>
             <h5>Votación del</h5>
             <h1 className='text-center'>Presupuesto Participativo General Pueyrredon 2022</h1>
-            <p>Gracias por participar de la votacion del presupuesto participativo 2022</p>
+            <p>Gracias por participar de la votación del presupuesto participativo 2022</p>
         </div>
         <div className='wrapper text-center'>
             <button onClick={changeStep} className='boton-comenzar'>Comenzar</button>
             <p>
-                Si aun no has visto los proyectos a votar, puedes verlos en mas detalle <a href="/propuestas" target="_blank">aqui</a>.
+                Si aun no has visto los proyectos a votar <a href="/propuestas" target="_blank">podés verlos en mas detalle aquí</a>.
             </p>
         </div>
     </div>
