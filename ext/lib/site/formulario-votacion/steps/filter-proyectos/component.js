ad74590059f490839fa5b0dd1bb8ef1670050fbd
Mariano Valdez <marianovaldez92@protonmail.com>
2022-08-03 10:17:39 -0300

    quito temporalmente filtro no terminado de nombre

--- ext/lib/site/formulario-votacion/steps/filter-proyectos/component.js
+++ ext/lib/site/formulario-votacion/steps/filter-proyectos/component.js
@@ -125,14 +125,14 @@ export default class FilterProyectos extends Component {
             handleFilter={this.handleFilter}
             clearFilter={this.clearFilter}
             />            
-          {topics && topics.length > 0 && <Select
+          {/* {topics && topics.length > 0 && <Select
             value={this.props.activeTitulo}
             onChange={(e) => this.handleFilter('activeTitulo', e.value)}
             placeholder='Buscá un proyecto por nombre'
             isSearchable={true}
             className='voto-search-name'
             options={topics.map((topic) => ({label: `#${topic.attrs && topic.attrs.numero} ${topic.mediaTitle}`, value: topic.id}))}
-            />}
+            />} */}
                 
         </div>
 
