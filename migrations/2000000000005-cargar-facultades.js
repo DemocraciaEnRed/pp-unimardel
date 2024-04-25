8c8e543d05fd07d80755598100bafdd0eb3b6ebe
xtian7489 <xtian7489@gmail.com>
2024-04-25 16:23:08 -0300

    agrega claustros schema

--- migrations/2000000000005-cargar-facultades.js
+++ migrations/2000000000005-cargar-facultades.js
@@ -1,23 +1,29 @@
 const dbReady = require('lib/models').ready
 const models = require('lib/models')
 
-const nombreMigrationParaLog = 'cargar facultades'
+const nombreMigrationParaLog = 'cargar facultades y claustros'
 const Facultad = models.Facultad
+const Claustro = models.Claustro
 
 const facultades = [
-
-  { nombre: 'CNAI'},
-  { nombre: 'ESM'},
-  { nombre: 'F.PSICO'},
-  { nombre: 'FAUD'},
-  { nombre: 'FCA'},
-  { nombre: 'FCEyN'},
-  { nombre: 'FCEyS'},
-  { nombre: 'FCSYTS'},
-  { nombre: 'FD'},
-  { nombre: 'FH'},
-  { nombre: 'FI'},
-  { nombre: 'Rec'},
+  { nombre: 'Arquitectura, Urbanismo y Diseño', abreviacion: 'FAUD' },
+  { nombre: 'Facultad de Ciencias Agrarias', abreviacion: 'FCA' },
+  { nombre: 'Facultad de Ciencias de la Salud y Trabajo Social', abreviacion: 'FCSYTS' },
+  { nombre: 'Facultad de Ciencias Económicas y Sociales', abreviacion: 'FCEyS' },
+  { nombre: 'Facultad de Ciencias Exactas y Naturales', abreviacion: 'FCEyN' },
+  { nombre: 'Facultad de Derecho', abreviacion: 'FD' },
+  { nombre: 'Facultad de Humanidades', abreviacion: 'FH' },
+  { nombre: 'Facultad de Ingeniería', abreviacion: 'FI' },
+  { nombre: 'Facultad de Psicologia', abreviacion: 'F.PSICO' },
+  { nombre: 'Escuela Superior de Medicina', abreviacion: 'ESM' },
+  { nombre: 'Colegio Nacional Arturo Illia', abreviacion: 'CNAI' },
+  { nombre: 'Rectorado', abreviacion: 'Rec' },
+]
+const claustros = [
+  { nombre: 'Estudiantes' },
+  { nombre: 'Docentes' },
+  { nombre: 'Nodocentes' },
+  { nombre: 'Graduados/as' },
 ]
 
 /**
@@ -40,9 +46,22 @@ exports.up = function up (done) {
         })
       })
     })
+    .then(() => {
+      return new Promise((resolve, reject) => {
+        Claustro.collection.count({}, (err, count) => {
+          if (err) reject(new Error(err))
+          if (count) {
+            console.log('Ya hay claustros cargados (%s), salteando migración', count)
+            reject(new SaltearPromises())
+          }
+          resolve()
+        })
+      })
+    })
 
     // Agregamos data
     .then(() => Facultad.collection.insertMany(facultades))
+    .then(() => Claustro.collection.insertMany(claustros))
 
     // Todo OK
     .then(() => {
